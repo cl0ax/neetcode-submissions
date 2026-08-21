9f9e91d15fe8254b6030a0f3c776ffaf2071dfe1
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-08-20 19:40:27 -0500

    Add: evaluate-reverse-polish-notation - submission-3

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new DequeArray<>();
+
+        for(int i = 0; i < tokens.length; i++){
+            String token = tokens[i];
+            if( token.isDigit() ){
+                stack.
+            }
+        }
+
+    }
+}
