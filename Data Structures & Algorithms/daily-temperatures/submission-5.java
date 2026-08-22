4781ef7076c514a0ba0373f5384dff5af1f5d968
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-08-21 20:24:30 -0500

    Add: daily-temperatures - submission-5

---
+++ Data Structures & Algorithms/daily-temperatures/submission-5.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int[] result = new int[temperatures.length];
+
+        int i = 0;
+        while(i < temperatures.length ){
+            int currT = temperatures[i];
+            int count = 0;
+
+            for(int j = i + 1; j < temperatures.length; j++){
+                
+                int futureT = temperatures[j];
+
+                if(futureT > currT ){
+                    count++;
+                    result[i] = count;
+                    break;
+                }
+                //result[i] = count;
+            }
+            i++;
+        }
+        return result;
+    }
+}
