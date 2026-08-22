113c9b24ea1ccd4ce8927765f2f507c1b7ca5272
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-08-21 19:51:05 -0500

    Add: evaluate-reverse-polish-notation - submission-28

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-28.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int sum = 0;
+
+        for(int i = 0; i < tokens.length; i++){
+            String token = tokens[i];
+            // if op
+            if( token.equals("*") ){
+                int a = stack.pop();
+                int b = stack.pop();
+                sum = a * b;
+                stack.push(sum);
+            }
+            else if( token.equals("/") ){
+                int a = stack.pop();
+                int b = stack.pop();
+                sum = a / b;
+                stack.push(sum);
+            }
+            else if( token.equals("+") ){
+                int b = stack.pop();
+                int a = stack.pop();
+                sum = a + b;
+                stack.push(sum);
+            }
+            else if( token.equals("-") ){
+                int b = stack.pop();
+                int a = stack.pop();
+                sum = a - b;
+                stack.push(sum);
+            }
+            else{ //is number to push onto stack
+                int num = Integer.parseInt(token);
+                stack.push(num);
+            }
+        }
+        return sum;
+    }
+}
