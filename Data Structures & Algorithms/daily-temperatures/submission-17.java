b2c2d6eb60348e0068ac93cfc0ae937fa51f5674
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-08-24 14:57:04 -0500

    Add: daily-temperatures - submission-17

---
+++ Data Structures & Algorithms/daily-temperatures/submission-17.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<int[]> stack = new ArrayDeque<>();
+
+        int[] result = new int[temperatures.length];
+
+        for(int i = 0; i < temperatures.length; i++) {
+            int temp = temperatures[i];
+
+            if( stack.isEmpty() || temp > stack.peek()[1] ) {
+                if(stack.isEmpty() ){
+                    stack.push(new int[]{i, temp});
+                }
+                else{
+                    int[] arr = stack.pop();
+                    int lastDay = arr[0];
+                    result[i] = i - lastDay;
+                }
+            }
+        }
+        return result;
+    }
+}
