7e0a75f4a2e221cdd30ed95538a2eab14fe958c8
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-08-28 19:48:42 -0500

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int n = position.length; 
+        double driving_rate = 0; 
+        int car_Fleet = 0;
+
+        for(int i = 0; i < n; i++) {
+            int diff = target - position[i]; 
+            int mph = speed[i];
+            int r = diff / mph; 
+            while( !stack.isEmpty() && r == target ){
+                car_Fleet++;
+            }
+            stack.push(i);
+        }
+        return car_Fleet;
+    }
+}
