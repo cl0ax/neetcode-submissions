3cdb06948cb16c2a0ee4211229751ee314f00130
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-08-30 19:56:11 -0500

    Add: string-encode-and-decode - submission-27

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-27.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        //String str = "";
+        for(String str : strs) {
+            //StringBuilder sb = new StringBuilder();
+            int len = str.length();
+            //str.append("#" + len + strs[i];)
+            str += "#" + len + str; 
+            return str; 
+        }
+
+        
+    }
+
+    public List<String> decode(String str) {
+        
+    }
+}
