5cef4f71582402dac7fa346445851e7b06d9dde6
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-08-30 19:58:49 -0500

    Add: string-encode-and-decode - submission-30

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-30.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        String str1 = "";
+        for(String str : strs) {
+            //StringBuilder sb = new StringBuilder();
+            int len = str.length();
+            //str.append("#" + len + strs[i];)
+            str1 += len + "#"  + str; 
+            
+            
+        }   
+        return str1; 
+    }
+    public List<String> decode(String str) {
+        
+    }
+}
