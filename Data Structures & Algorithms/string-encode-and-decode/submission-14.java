69b072da923452e5d53ccf288333a1e8cee62666
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-08-30 19:46:14 -0500

    Add: string-encode-and-decode - submission-14

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-14.java
@@ -0,0 +1,13 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        Strin str = "";
+        for(int i = 0; i < strs.length; i++) {
+            str += strs[i];
+        }
+    }
+
+    public List<String> decode(String str) {
+
+    }
+}
