2b28ba7493c4870654488ba23a0a8b48535d9ab8
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-08-31 20:51:47 -0500

    Add: car-fleet - submission-32

---
+++ Data Structures & Algorithms/car-fleet/submission-32.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length; 
+        Deque<Double> stack = new ArrayDeque<>();
+
+        int[][] cars = new int[n][2];
+
+        for(int i = 0; i < n; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        }
+        Arrays.sort(cars, (a, b) -> b[0] - a[0]);
+
+        for(int[] car : cars ){
+            double time = (double) target - car[0]; 
+
+            Double eta =  (double) time / car[1];
+
+            if( stack.isEmpty() || eta > stack.peek() ){
+                stack.push(eta);
+            }
+            
+        }
+
+        return stack.size();
+    }
+}
