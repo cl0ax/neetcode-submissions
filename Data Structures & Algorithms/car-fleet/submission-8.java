f2eaead73c1ca4c708a51cdbaa12c643d27aabfa
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-08-31 20:29:12 -0500

    Add: car-fleet - submission-8

---
+++ Data Structures & Algorithms/car-fleet/submission-8.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length; 
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        int[][] cars = new int[n][2];
+
+        for(int[] car : position) {
+            cars[i] = position[i];
+            cars[i][1] = speed[i];
+        }
+
+
+    }
+}
