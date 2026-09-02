b78d75a21703585762f6e08792e666a9cab59bba
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-09-02 18:31:47 -0500

    Add: asteroid-collision - submission-50

---
+++ Data Structures & Algorithms/asteroid-collision/submission-50.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int a : asteroids) {
+            while (!stack.isEmpty() && stack.peek() > 0 && a < 0) {
+                if (Math.abs(a) > Math.abs(stack.peek())) {
+                    int popped = stack.pop();
+                    stack.push(a);
+                } else if (Math.abs(a) == Math.abs(stack.peek())) {
+                    stack.pop();
+                }
+            }
+            if( stack.isEmpty() ) {
+                stack.push(a);
+            }
+        }
+
+        int[] result = new int[stack.size()];
+        for (int i = stack.size() - 1; i > 0; i--) {
+            result[i] = stack.pop();
+        }
+
+        return result;
+    }
+}
