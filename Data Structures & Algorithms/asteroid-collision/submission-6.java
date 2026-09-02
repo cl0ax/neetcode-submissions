c41319135d1bcc77d6781f137fc70c46f29c440a
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-09-01 19:35:01 -0500

    Add: asteroid-collision - submission-6

---
+++ Data Structures & Algorithms/asteroid-collision/submission-6.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int direction = 0;
+        for( int asteroid : asteroids ) {
+            //direction 
+            if( Math.signum(asteroid) == 1) { //positive
+                direction = 1; 
+            }
+            else{
+                direction = -1;
+            }
+
+            //which one is bigger than the other 
+            if( !stack.isEmpty() && Math.signum(asteroid) != Math.signum(stack.peek() ) ){
+                if( asteroid > stack.peek() ) {
+                    int exploded = stack.pop();
+                    stack.push( asteroid );
+                }
+            }
+
+            if( stack.isEmpty() || asteroid > stack.peek() && Math.abs(asteroid) != Math.abs( stack.peek() )) {
+                stack.push(asteroid);
+            }
+        }
+
+        int[] result = new int[stack.size()];
+
+        for(int i = 0; i < stack.size(); i++ ) {
+            result[i] = stack.pop();
+        }
+
+
+        return result;
+    }
+}
