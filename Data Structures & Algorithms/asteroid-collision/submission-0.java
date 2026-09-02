367513ff3f04258eacde7e4ff96c25fb8d03a049
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-09-01 19:14:31 -0500

    Add: asteroid-collision - submission-0

---
+++ Data Structures & Algorithms/asteroid-collision/submission-0.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for( int asteroid : asteroids ) {
+            
+        }
+    }
+}
