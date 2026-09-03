ea9e8c5c313f9a8332fae1229fd2738f989feeb7
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-09-03 15:59:33 -0500

    Add: decode-string - submission-7

---
+++ Data Structures & Algorithms/decode-string/submission-7.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String decodeString(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        String decoded_str = "";
+        int i = 0;
+        int times = 1;
+
+        // while( i < s.length() ){
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if( Character.isLetter(c) ) {
+                stack.push(c);
+            }
+
+            if ( Character.isDigit(c)) {
+                times = Character.getNumericValue(c);
+                stack.push(c);
+                
+            }
+
+            if( c == ']') {
+                char popped = stack.pop();
+                if( popped == '[]') {
+                    decoded_str += 
+                }
+            }
+        }
+        // i++;
+        //}
+        return decoded_str;
+    }
+}
