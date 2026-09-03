2dae253f792b54d625a13f4fcf36fcb1102decb2
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-09-03 15:46:35 -0500

    Add: decode-string - submission-0

---
+++ Data Structures & Algorithms/decode-string/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String decodeString(String s) {
+
+        Deque<Character> stack = new ArrayDeque<>();
+        String decoded_str = "";
+        int i = 0;
+        int times = 1;
+
+        while( i < s.length() ){
+
+            for(int j = i + 1; j < s.length(); j++ ) {
+                char c = s.charAt(j);
+
+                if( Character.isDigit(c) ) {
+                    
+                }
+            }
+            i++;
+        }
+        return decoded_str
+    }
+}
