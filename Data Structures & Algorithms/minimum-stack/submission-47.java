905daa38d3ee1b51ea23b0d0fb28bc7cb2906024
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-09-03 15:24:18 -0500

    Add: minimum-stack - submission-47

---
+++ Data Structures & Algorithms/minimum-stack/submission-47.java
@@ -0,0 +1,32 @@
+class MinStack {
+
+    private Deque<Integer> stack = new ArrayDeque<>();
+    private Deque<Integer> minStack = new ArrayDeque<>();
+
+    public MinStack() { //constructor
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if( !stack.isEmpty() || val < minStack.peek() ) {
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        int popped = stack.pop();
+        if( popped == minStack.peek() ) {
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
