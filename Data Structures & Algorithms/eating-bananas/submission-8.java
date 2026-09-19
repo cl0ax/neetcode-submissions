c171d7980ca3d34e52892a7e46eeb9b7c477f78f
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-09-19 10:08:19 -0500

    Add: eating-bananas - submission-8

---
+++ Data Structures & Algorithms/eating-bananas/submission-8.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int max = 0;
+        for (int pile : piles) {
+            max = Math.max(max, pile);
+        }
+
+
+        int left = 1;
+        int right = max;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            for (int pile : piles) {
+                long total += pile / mid;
+                if (pile % mid != 0) {
+                    total++;
+                }
+            }
+            if(total <= h ) {
+                right = mid - 11;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return left; 
+    }
+}
