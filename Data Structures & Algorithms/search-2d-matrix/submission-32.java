8338aa5607e97a2693d570e843b134b398b46283
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-09-24 18:33:11 -0500

    Add: search-2d-matrix - submission-32

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-32.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int top = 0;
+        int bottom = matrix.length - 1; 
+
+        while( top <= bottom ) {
+            int row = top + (bottom - top) / 2; 
+
+            if( matrix[row][0] < target ) {
+                top = row + 1; 
+            }
+            else if( matrix[row][matrix.length - 1] > target ) {
+                bottom = row - 1; 
+            }
+            else{ // == target
+                int left = 0;
+                int right = matrix[row].length - 1;
+
+                while( left <= right ) {
+                    int col = left + (right - left) / 2;
+
+                    if( matrix[row][col] == target ) {
+                        return true; 
+                    }
+                    else if( matrix[row][col] > target ) {
+                        right = col - 1;
+                    }
+                    else{
+                        left = col + 1; 
+                    }
+                }
+                return false; 
+            }
+        }
+        return false; 
+    }
+}
