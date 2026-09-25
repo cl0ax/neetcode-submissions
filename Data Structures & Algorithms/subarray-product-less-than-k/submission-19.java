41ad2d28c42f1d1db2fb216c3609e7009ef6dd8c
Abraham <147194673+cl0ax@users.noreply.github.com>
2026-09-25 09:28:14 -0500

    Add: subarray-product-less-than-k - submission-19

---
+++ Data Structures & Algorithms/subarray-product-less-than-k/submission-19.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (nums.length <= 0) {
+            return 0;
+        }
+
+        int count = 0;
+        int left = 0;
+
+        int product = 1;
+        for (int right = 0; right < nums.length; right++) {
+            product *= nums[right];
+            while (product >= k && left <= right) {
+                product /= nums[left];
+                left++;
+            }
+            count += left + (right - left);
+        }
+        return count;
+    }
+}
